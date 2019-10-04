41b069626aeedfa7d825abbdfb73705c49f4aace
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-10-04 20:39:23 +0700

    implementasi keyword dan method yang static

--- static.php
+++ static.php
@@ -4,11 +4,16 @@ class ContohStatic {
 	public static $angka = 1 ;
 
 	public static function hallo () {
-		return "Hallo.";
+		return "Hallo " . self::$angka . "kali.";
 	}
 }
 
 echo ContohStatic ::$angka;
+echo "<br>";
+echo ContohStatic ::hallo();
+echo "<hr>";
+echo ContohStatic::hallo();
+
 
 
 
