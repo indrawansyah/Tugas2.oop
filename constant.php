8cfeedfa3acf6e934b4cf52de3f3c64407cea426
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-10-04 21:43:41 +0700

    membuat constanta dan contoh implementasi magic constant

---
+++ constant.php
@@ -0,0 +1,49 @@
+<?php 
+
+// define("NAMA", "Indrawansyah");
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 32;
+// echo UMUR;
+
+// class Coba {
+// 	const NAMA = "Indrawansyah";
+// }
+
+// echo Coba :: NAMA;
+
+//echo __FILE__;
+
+
+
+
+// function coba (){
+// 	return __FUNCTION__;
+// }
+
+// echo coba ();
+
+
+class Coba {
+	public $kelas = __CLASS__;
+}
+$obj = new Coba;
+echo $obj->kelas;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
